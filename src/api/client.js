16410022708e049f0a454f9415543cc032824f8e
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2026-03-30 12:34:34 +0300

    Add CSRF support

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,22 @@
 import axios from "axios";
 
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
 const api = axios.create({
   baseURL: "https://hatabox-first.onrender.com/api/",
   withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const csrftoken = getCookie("csrftoken");
+  if (csrftoken) {
+    config.headers["X-CSRFToken"] = csrftoken;
+  }
+  return config;
 });
 
 export default api;

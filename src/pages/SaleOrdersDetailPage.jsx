a4c4e7dd1b221006e4f0c987097cc54025be8091
Iurii Biriukov <wingsofphoenix.2013@gmail.com>
2026-05-20 19:09:35 +0300

    размер шрифта

--- src/pages/SaleOrdersDetailPage.jsx
+++ src/pages/SaleOrdersDetailPage.jsx
@@ -614,7 +614,7 @@ function SaleOrdersDetailPage() {
                 <Text
                   strong
                   style={{
-                    fontSize: 40,
+                    fontSize: 120,
                     lineHeight: '42px',
                     textAlign: 'center',
                     wordBreak: 'break-word',

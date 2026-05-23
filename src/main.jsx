f38d3eb32de6f62c6094028a33d6a17cd70821af
Iurii Biriukov <wingsofphoenix.2013@gmail.com>
2026-05-23 23:07:44 +0300

    настройки календаря

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import ukUA from 'antd/locale/uk_UA';
+import dayjs from 'dayjs';
+import 'dayjs/locale/uk';
+
 import App from './App';
 import 'antd/dist/reset.css';
 
+dayjs.locale('uk');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ConfigProvider locale={ukUA}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ConfigProvider>,
 );
